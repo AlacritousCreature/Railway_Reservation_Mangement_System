cb5e66b11437c26a6b9d8b602e982715ee3ac7d9
AlacritousCreature <IIT2019007@iiita.ac.in>
2021-04-27 21:15:48 +0530

    ganja project added

---
+++ railway/new_user_form.php
@@ -0,0 +1,32 @@
+<html>
+
+<body style=" background-image: url(userlogin.png);
+    height: 100%; 
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;">
+
+    <?php
+
+    require "db.php";
+
+    $pwd = $_POST["password"];
+    $eid = $_POST["emailid"];
+    $mno = $_POST["mobileno"];
+    $dob = $_POST["dob"];
+
+    $sql = "INSERT INTO user (password,emailid,mobileno,dob) VALUES ('" . $pwd . "','" . $eid . "','" . $mno . "','" . $dob . "')";
+    // echo $sql;
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Hi $eid, <a href=\"index.htm\"> Click here </a> to browse through our website!!! ";
+    } else {
+        echo "Error:" . $conn->error . "<br> <a href=\"new_user_form.html\">Go Back to Login!!!</a> ";
+    }
+
+    $conn->close();
+    ?>
+
+</body>
+
+</html>
